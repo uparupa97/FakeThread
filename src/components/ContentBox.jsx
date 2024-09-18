eaa5c3f30edff4a8156aa31222db4ee3ab294131
turtle <uparupa97@icloud.com>
2024-09-18 15:58:45 +0900

    home post without icon on

--- src/components/ContentBox.jsx
+++ src/components/ContentBox.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import profileImage from '/Users/mac/Desktop/ project-visualstudiocode/LDuparupa97/threadapp/src/image/uparupa.png';
 
-const ContentBox = () => {
+const ContentBox = ({ profileImage, name, textMessage }) => {
   return (
     <div className="px-3 py-2 flex flex-row">
       <img
         src={profileImage}
         alt="profile"
-        className="w-20 h-20 mt-2 rounded-full shadow-md bg-slate-800 overflow-hidden"
+        className="w-14 h-14 mt-2 rounded-full shadow-md bg-slate-800 overflow-hidden"
       />
 
       <div className="flex flex-col text-left w-full">
-        <p className="my-1 ml-4 font-bold text-xl">Uparupa</p>
-        <p className="w-full h-[400px] m-4 rounded-lg bg-slate-800"></p>
+        <p className="mt-2 ml-4 font-bold text-xl">{name}</p>
+        <p className="w-full m-4 rounded-lg bg-slate-800 p-3">{textMessage}</p>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,35 @@ const Home = () => {
     history('/login');
   };
 
+  const messagecontent = [
+    {
+      profileImage:
+        'https://images.wagwalkingweb.com/media/breed/maltese/appearance/maltese.png',
+      idName: 'Uparupa',
+      textMessage: `The subway is not fancy—in fact, riding it
+can suck with rat, smells and all lol—but
+there’ll never be anything more iconic?
+and effective for getting around the city,
+especially during NYFW.`,
+    },
+    {
+      profileImage:
+        'https://d128mjo55rz53e.cloudfront.net/media/images/blog-breed-french_pomeranian_2.max-400x400.format-jpeg.jpg',
+      idName: 'steamylit',
+      textMessage: `NO ONE TOLD ME IT WAS NATIONAL
+READ A BOOK DAY? WHY AM I WORKING?
+Well if you need a book you know we
+have a selection 🤭🤗`,
+    },
+    {
+      profileImage:
+        'https://lh5.googleusercontent.com/proxy/RGyWWIOJiMuNmTOeWWkfeEc4MX30g1I62M4E0CtvR0LWOTQ5zrrShJjrIfksUHMqC5KWeiFNWbgqkEAdRh91f9qg5-Rh43W0DFyrC_nPkEm3s-lw',
+      idName: 'thespeakershack',
+      textMessage: `AVID HiFi Integra amplifier and EVO
+Three speakers 🔊❤️🇬🇧🙏`,
+    },
+  ];
+
   return (
     <div className="px-8 pt-6 w-[600px] max-w-xl md:max-w-2xl lg:max-w-3xl mx-auto">
       <div className="grid grid-cols-3 my-2">
@@ -29,9 +58,21 @@ const Home = () => {
       </div>
 
       <div>
-        <ContentBox />
-        <ContentBox />
-        <ContentBox />
+        <ContentBox
+          profileImage={messagecontent[0].profileImage}
+          name={messagecontent[0].idName}
+          textMessage={messagecontent[0].textMessage}
+        />
+        <ContentBox
+          profileImage={messagecontent[1].profileImage}
+          name={messagecontent[1].idName}
+          textMessage={messagecontent[1].textMessage}
+        />
+        <ContentBox
+          profileImage={messagecontent[2].profileImage}
+          name={messagecontent[2].idName}
+          textMessage={messagecontent[2].textMessage}
+        />
       </div>
 
       <div className="flex items-center">
