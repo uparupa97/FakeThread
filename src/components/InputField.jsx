f0c36b91022054e992685d6c81fe9fbd30a4871e
turtle <uparupa97@icloud.com>
2024-09-19 11:46:13 +0900

    loginsubmit

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const InputField = ({ typeInput, inputValue, inputText, onChange }) => {
+const InputField = ({ typeInput, inputText, onChange }) => {
   const [valueid, SetValueid] = useState('');
 
   const inputChange = (event) => {
-    console.log('event', event.target.value);
     const { value } = event.target; // value = event.target.value랑 동일한 의미임
     SetValueid(value);
-    onChange(value, inputValue);
+    onChange(value, typeInput);
   };
 
   return (
@@ -16,7 +15,6 @@ const InputField = ({ typeInput, inputValue, inputText, onChange }) => {
         required
         type={typeInput}
         onChange={inputChange} //해당값이 변경했을 떄 일어나는 것
-        name={inputValue}
         value={valueid}
         placeholder={inputText}
         className="bg-slate-500 block rounded-lg border border-gray-800 w-full p-2 mt-4"
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,66 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import InputField from '../components/InputField';
 import LoginButton from '../components/LoginButton';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Threadtext from '../components/Threadtext';
 import uparupa from '/Users/mac/Desktop/ project-visualstudiocode/LDuparupa97/threadapp/src/image/chuzlogo.svg';
 import SocialButton from '../components/SocialButton';
 
 const Login = () => {
-  const handleInputChange = (data) => {
-    console.log('handleinputchange', data);
+  const [loginUser, setLoginUser] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (data, typeInput) => {
+    console.log('handleinputchange', data, typeInput);
+
+    let newFormdata = { ...loginUser, [typeInput]: data };
+    setLoginUser(newFormdata);
   };
-  // const history = useNavigate();
 
-  // const [inputEmail, setInputEmail] = useState('email');
-  // const [inputPassword, setInputPassword] = useState('password');
-
-  // const [loginUser, setLoginUser] = useState({
-  //   email: '',
-  //   password: '',
-  // });
-
-  // const handleInputChange = (e) => {
-  //   const { name, value } = e.target;
-  //   setLoginUser({
-  //     ...loginUser,
-  //     [name]: value,
-  //   });
-  // };
-
-  // const handleEvent = (e) => {
-  //   e.preventDefault();
-
-  //   const storedUser = JSON.parse(localStorage.getItem('user'));
-
-  //   const foundUser = storedUser.find((user) => user.email === loginUser.email);
-
-  //   if (foundUser) {
-  //     if (foundUser.password === loginUser.password) {
-  //       setLoginUser({ name: '', email: '', password: '' });
-  //       alert('로그인 성공!');
-  //       history('/');
-  //     } else {
-  //       alert('비밀번호가 틀렸습니다.');
-  //       setLoginUser({ name: '', email: '', password: '' });
-  //       setInputEmail('');
-  //       setInputPassword('');
-  //     }
-  //   } else {
-  //     alert('해당되는 사용자는 없습니다.');
-  //     setLoginUser({ name: '', email: '', password: '' });
-  //     setInputEmail('');
-  //     setInputPassword('');
-  //   }
-  // };
+  useEffect(() => {
+    console.log('formdata', loginUser);
+  }, [loginUser]);
 
   return (
     <div className="pt-36 px-10 w-[600px]">
       <img src={uparupa} alt="안녕하세요" className="w-40 h-auto m-auto" />
       <Threadtext />
-      <form id="login-form" onSubmit={handleEvent}>
+      <form id="login-form">
         <InputField
-          typeInput={'text'}
+          typeInput={'email'}
           onChange={handleInputChange}
           inputText={'Email'}
         />
